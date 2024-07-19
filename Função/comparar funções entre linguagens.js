9a2821e6d6f2dfe5d73e9ec5d65637a094c1a636
Pedro Augusto Barbosa Pereira <peteraugustobp@gmail.com>
2024-07-19 00:04:13 -0300

    adicionei um pouco mais de explicações e um mais de informações sobre funções

---
+++ Função/comparar funções entre linguagens.js
@@ -0,0 +1,53 @@
+
+function enviarDados(){
+    let Bradesco = "Banco de dados"
+    console.log("salvando dados em:" + Bradesco)
+
+    if (3 === 3)
+        console.log("senha validada")
+}
+
+enviarDados()
+
+//assim fica em formato Java
+
+/*public class Main {
+    public static void main(String[] args) {
+        enviarDados();
+    }
+
+    static void enviarDados() {
+        String Bradesco = "Banco de dados";
+        System.out.println("Salvando dados em: " + Bradesco);
+
+        if (3 == 3) {
+            System.out.println("Senha validada");
+        }
+    }
+}
+*/
+
+//ele criou uma classe chamada main pra declarar esse codigo, agora em C# abaixo
+
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        EnviarDados();
+    }
+
+    static void EnviarDados()
+    {
+        string Bradesco = "Banco de dados";
+        Console.WriteLine("Salvando dados em: " + Bradesco);
+
+        if (3 == 3)
+        {
+            Console.WriteLine("Senha validada");
+        }
+    }
+}
+*/
--- Função/cuidados com função.js
+++ Função/cuidados com função.js
@@ -1,1 +1,32 @@
-//evitar de colocar numeros em nome de função
+
+//evitar de colocar numeros e espaços em nomes de função, função são ações
+
+function Getdata(){
+    //logica implementada
+    console.log("Pegando dados do usúario")
+    if(1 < 3){
+        console.log("Numero encontrado")
+    }
+}
+
+function CheckValue(){
+    //checar valores de algo do banco
+    console.log("Validando Dados....")
+}
+
+function sendToDatabase(){
+    //enviar dados para a base de dados
+    console.log("Cadastrando Dados....")
+}
+
+//cada função tem q fazer a mesma função com o q o nome condiz
+//ou se quiser pode colocar apenas uma função para tudo que chama todas as outras funções
+
+function mainSaveData(){
+    Getdata()
+    CheckValue()
+    sendToDatabase()
+}
+
+mainSaveData()
+4
